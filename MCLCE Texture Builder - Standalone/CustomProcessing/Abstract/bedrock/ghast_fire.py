394dd36ac0702df8f13d346464831342dd4a529d
Leviah <nightdrago9893@gmail.com>
2026-08-18 23:48:14 -0400

    ghast_fire Bug
    Fixed a bug where ghast_fire would sometimes fail to convert

--- MCLCE Texture Builder - Standalone/CustomProcessing/Abstract/bedrock/ghast_fire.py
+++ MCLCE Texture Builder - Standalone/CustomProcessing/Abstract/bedrock/ghast_fire.py
@@ -7,5 +7,5 @@ from CodeLibs.Path import Path
 class ghast_fire(Custom.Function):
     def createImage(self):
         return ut.getImageNoOpacity(
-                rd.readImageSingular(self.wiiuName, Path("ghast", "ghast_shooting").getPath(), "entity", ut.size(ut.mobside, (ut.mobside / 2)))
-            )
+            rd.readImageSingular(self.wiiuName, Path("ghast", "ghast_shooting").getPath(), "entity", ut.size(ut.mobside, (ut.mobside / 2)))
+        )
